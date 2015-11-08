3b4351b49ee7ce4913f1fd880675d8458debbdcf
satvik <satvik.jagannath7@gmail.com>
2015-11-09 00:20:40 +0530

    enhanced script

--- connect-android.sh
+++ connect-android.sh
@@ -1,2 +1,6 @@
+echo "Please connect your Device before running this script"
+adb devices
+echo "Starting React Native Server"
 react-native start > /dev/null 2>&1 &
+echo "adb reverse"
 adb reverse tcp:8081 tcp:8081
